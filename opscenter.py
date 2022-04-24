93951dc4f5fc5020cc61602db172bbd7f2273f61
Bradley Whitebread <bradley@deliverycircle.com>
2022-04-24 11:17:18 -0400

    Added checks for Team 3 Puzzles

--- opscenter.py
+++ opscenter.py
@@ -17,9 +17,9 @@ layout = [
     [sg.Text(h, size=(10,1), font='_ 18') for h in headings],
 
     # Should only have to make edits below for your puzzles
-    [sg.Text('Puzzle 1', size=(16,1)), sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Light', size=(14,1), text_color='Red', key='chk_light'), sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Checkbox', size=(0,1))],
-    [sg.Text('Puzzle 2', size=(16,1)), sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Water', size=(14,1), text_color='Red', key='chk_water'),  sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Checkbox', size=(0,1))],
-    [sg.Text('Puzzle 3', size=(16,1)), sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Fire', size=(14,1), text_color='Red', key='chk_fire'),  sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Checkbox', size=(0,1))],
+    [sg.Text('Puzzle 1', size=(16,1)), sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Light', size=(14,1), text_color='Red', key='chk_light'), sg.Checkbox('Sound Puzzle', size=(13,1), text_color='Red', key='chk_sound'), sg.Checkbox('Checkbox', size=(0,1))],
+    [sg.Text('Puzzle 2', size=(16,1)), sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Water', size=(14,1), text_color='Red', key='chk_water'),  sg.Checkbox('Flow Puzzle', size=(13,1), text_color='Red', key='chk_flow'), sg.Checkbox('Checkbox', size=(0,1))],
+    [sg.Text('Puzzle 3', size=(16,1)), sg.Checkbox('Checkbox', size=(13,1)), sg.Checkbox('Fire', size=(14,1), text_color='Red', key='chk_fire'),  sg.Checkbox('Morse Puzzle', size=(13,1), text_color='Red', key='chk_morse'), sg.Checkbox('Checkbox', size=(0,1))],
      
     [sg.Button('Refresh', size=(81,2))]
 ]
@@ -40,15 +40,25 @@ while True:
     # End program if user closes window
     if event == sg.WIN_CLOSED:
         break
-    
+   
     # This is where we start looking for puzzle completion messages
     # How to get values of a box: values['chk_light']
     data = Log.getstatus_json()    #Is updated on Refresh; wont need this unless eve server is up
     for i in data["_items"]:    # Will crash if no connection to eve
+       
+        # Team 2 Puzzles
         if(i["message"] == "Water Puzzle Completed"):
             updatechkbox('chk_water')
         if(i["message"] == "Light Puzzle Completed"):
             updatechkbox('chk_light')
         if(i["message"] == "Fire Puzzle Completed"):
             updatechkbox('chk_fire')
+       
+        # Team 3 Puzzles
+        if(i["message"] == "Sound Puzzle Completed"):
+            updatechkbox('chk_sound')
+        if(i["message"] == "Flow Puzzle Completed"):
+            updatechkbox('chk_flow')
+        if(i["message"] == "Morse Puzzle Completed"):
+            updatechkbox('chk_morse')
 window.close()
